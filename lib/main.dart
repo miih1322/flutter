7de6f0c03169a05ca794176e938a3eee91663734
miih1322 <nevascapicolodias1322@gmail.com>
2020-02-27 10:30:55 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+void main()=> runApp(
+  MaterialApp(
+    home: Center(
+      child: Container(
+        color: Colors.pink[400],
+        child: Text("O lado bom de ser gay.",
+          style: TextStyle(color: Colors.yellow[400]),
+        ),
+      ),
+    ),
+  ),
+);
+
